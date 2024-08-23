7119164439ba6cb1e62698f46e72defe24bb79cd
NilayPatel10721 <122658744+NilayPatel10721@users.noreply.github.com>
2024-08-24 00:32:15 +0530

    Add existing project files to Git

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -4,39 +4,65 @@ const Result = require("../models/model");
 
 const scrapeMultipleResults = async (req, res) => {
   try {
-    const { baseUrl, start, end } = req.body;
+    // const { baseUrl } = req.body;
 
-    if (!baseUrl || start == null || end == null) {
-      return res
-        .status(400)
-        .json({ message: "Base URL, start, and end values are required" });
-    }
+    const start = 1;
+    const end = 100;
+
+    // if (!baseUrl || start == null || end == null) {
+    //   return res
+    //     .status(400)
+    //     .json({ message: "Base URL, start, and end values are required" });
+    // }
 
     const results = [];
+    let consecutiveEmptyResults = 0;
+    const maxEmptyResults = 1;
 
     for (let i = start; i <= end; i++) {
+      if (consecutiveEmptyResults >= maxEmptyResults) {
+        console.log(
+          "Stopping the scraping process due to consecutive empty results."
+        );
+        break;
+      }
+
       const paddedNumber = i.toString().padStart(2, "0");
       const url = `https://ums.cvmu.ac.in/GenerateResultHTML/2877/42110${paddedNumber}.html`;
 
       console.log(`Scraping URL: ${url}`);
 
-      const { data } = await axios.get(url);
-      const $ = cheerio.load(data);
+      try {
+        const { data } = await axios.get(url);
+        const $ = cheerio.load(data);
 
-      const name = $("tr.background1 td").eq(8).text().trim();
-      const sgpa = $("tr.background1 td").eq(17).text().trim();
+        const name = $("tr.background1 td").eq(8).text().trim();
+        const sgpa = $("tr.background1 td").eq(17).text().trim();
 
-      if (name && sgpa) {
-        const studentData = { name, sgpa };
-        results.push(studentData);
+        if (name && sgpa) {
+          const studentData = { name, sgpa };
+          results.push(studentData);
+          consecutiveEmptyResults = 0;
+        } else {
+          consecutiveEmptyResults++;
+        }
+      } catch (error) {
+        console.error(
+          `Error occurred while scraping data from URL: ${url}`,
+          error
+        );
+        consecutiveEmptyResults++;
       }
     }
 
-    const savedResults = await Result.insertMany(results);
-
-    res.json(savedResults);
+    if (results.length > 0) {
+      const savedResults = await Result.insertMany(results);
+      res.json(savedResults);
+    } else {
+      res.status(404).json({ message: "No valid student data found" });
+    }
   } catch (error) {
-    console.error("Error occurred while scraping data from URL:", error);
+    console.error("Error occurred while scraping data:", error);
     res.status(500).send("Error occurred while scraping");
   }
 };
